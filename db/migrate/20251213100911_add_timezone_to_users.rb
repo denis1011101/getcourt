2ed38ca21fdd1cca92ba9715f9554cbfb12e8659
denis <denisdenis9331@gmail.com>
2025-12-17 23:47:38 +0500

    fix db:migrate

--- db/migrate/20251213100911_add_timezone_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddTimezoneToCourts < ActiveRecord::Migration[8.0]
-  def change
-    add_column :courts, :timezone, :string
-    add_index :courts, :timezone
-  end
-end
--- db/migrate/20251214122040_remove_timezone_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveTimezoneFromUsers < ActiveRecord::Migration[8.0]
-  def change
-    remove_index :users, :timezone if index_exists?(:users, :timezone)
-    remove_column :users, :timezone, :string
-  end
-end
